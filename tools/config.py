ebae9ccd235a15c9c750d8c444106bf7f7f940b1
Frankin Fu <zhiyongf@cn.ibm.com>
2017-08-10 17:44:15 +0800

    remove tools/config.py file.

--- tools/config.py
+++
@@ -1,5 +0,0 @@
-LOGIN_URL = 'https://login.aliexpress.com/buyer.htm?spm=2114.11010108.1000002.7.6bb2d449PlxkIR&return=https%3A%2F%2Fwww.aliexpress.com%2F&random=46C4127371C13A0ED2EA46652B3EAFC8'
-LOGIN_USER = '121697524@qq.com'
-LOGIN_PASSWD = 'q1w2e3r4'
-
-CHROMEDRIVER_PATH = '/Users/frankin/Downloads/chromedriver'
