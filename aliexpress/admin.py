9489dd29d8da2dce6835be52a6c3de3205b82885
Frankin Fu <zhiyongf@cn.ibm.com>
2017-08-16 22:56:34 +0800

    change site admin list display.

--- aliexpress/admin.py
+++ aliexpress/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Country, Query, QueryResult
 
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'lang_code', 'url')
+
 class QueryAdmin(admin.ModelAdmin):
     list_display = ('id', 'keywords', 'site', 'user', 'timestamp')
 
@@ -9,6 +12,6 @@ class QueryResultAdmin(admin.ModelAdmin):
     list_display = ('id', 'query', 'page', 'rank', 'overall_rank', 'product_code', 'title', 'price', 'orders', 'store')
     list_filter = ('query', )
 
-admin.site.register(Country)
+admin.site.register(Country, SiteAdmin)
 admin.site.register(Query, QueryAdmin)
 admin.site.register(QueryResult, QueryResultAdmin)
